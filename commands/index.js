928e66cdb1804ca93a9d3ce70f5b8fa3cf6837c1
OhBeomho <tyler0322@naver.com>
2023-08-12 15:04:43 +0900

    forEach function

--- commands/index.js
+++ commands/index.js
@@ -5,14 +5,14 @@ const commands = [];
 // 명령어 불러오기
 const commandFolders = fs.readdirSync(__dirname).filter((f) => f !== "index.js");
 
-for (let folder of commandFolders) {
+commandFolders.forEach((folder) => {
 	const commandsPath = path.join(__dirname, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
 
-	for (let file of commandFiles) {
+	commandFiles.forEach((file) => {
 		const command = require(path.join(commandsPath, file));
 		commands.push(command);
-	}
-}
+	});
+});
 
 module.exports = commands;
