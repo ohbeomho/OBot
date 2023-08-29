8f42a1b950d4342b41b9813ad01b75b953d6d418
OhBeomho <tyler0322@naver.com>
2023-08-29 18:23:14 +0900

    converting number to string

--- commands/etc/roll.js
+++ commands/etc/roll.js
@@ -6,6 +6,6 @@ module.exports = {
 		.setDescription("1부터 100까지의 수 중 무작위 숫자를 출력합니다."),
 	async execute(interaction) {
 		const randomNumber = Math.floor(Math.random() * 100 + 1);
-		await interaction.reply(randomNumber);
+		await interaction.reply(String(randomNumber));
 	}
 };
