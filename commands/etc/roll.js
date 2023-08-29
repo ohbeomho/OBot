a5d43d62ba67f46ab0d2b5ae4e22b1065133d595
OhBeomho <tyler0322@naver.com>
2023-08-29 18:19:26 +0900

    new command

---
+++ commands/etc/roll.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("roll")
+		.setDescription("1부터 100까지의 수 중 무작위 숫자를 출력합니다."),
+	async execute(interaction) {
+		const randomNumber = Math.floor(Math.random() * 100 + 1);
+		await interaction.reply(randomNumber);
+	}
+};
