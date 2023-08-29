190a0d45423b3f507d1493835928a1a29725961c
OhBeomho <tyler0322@naver.com>
2023-08-29 18:19:01 +0900

    changed color command option

--- commands/role/color.js
+++ commands/role/color.js
@@ -2,7 +2,6 @@ const {
 	SlashCommandSubcommandBuilder,
 	SlashCommandRoleOption,
 	SlashCommandStringOption,
-	Colors,
 	PermissionsBitField
 } = require("discord.js");
 const { ephemeralMessage } = require("../../utils");
@@ -20,12 +19,13 @@ module.exports = {
 		.addStringOption(
 			new SlashCommandStringOption()
 				.setName("color")
-				.setDescription("역할의 새로운 색")
+				.setDescription("역할의 새로운 색 (#rrggbb 형식)")
 				.setRequired(true)
 		),
 	async execute(interaction) {
 		const role = interaction.options.getRole("role");
 		let color = interaction.options.getString("color");
+		const previousColor = "#" + role.color.toString(16);
 
 		if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
 			await interaction.reply(
@@ -36,34 +36,18 @@ module.exports = {
 			return;
 		}
 
-		const errorMessage = {
-			content: `올바르지 않은 색입니다.
-https://old.discordjs.dev/#/docs/discord.js/main/typedef/ColorResolvable 여기를 참고해 주세요.`,
-			ephemeral: true
-		};
-
-		if (color) {
-			try {
-				color = JSON.parse(color);
-				if (Array.isArray(color) && color.length !== 3) {
-					await interaction.reply(errorMessage);
-				}
-			} catch (err) {
-				if (
-					(color.startsWith("#") && ![4, 5, 7].includes(color.length)) ||
-					(!color.startsWith("#") &&
-						!Colors[color.charAt[0].toUpperCase() + color.substring(1)])
-				) {
-					await interaction.reply(errorMessage);
-				}
-			}
-		}
-
-		if (interaction.replied) {
+		if (!color.startsWith("#") || color.length !== 7) {
+			await interaction.reply(
+				ephemeralMessage(
+					"색은 `#rrggbb` (hex string) 형식으로 입력되어야 합니다.\n예시: 빨강 = `#ff0000`"
+				)
+			);
 			return;
 		}
 
 		await role.edit({ color });
-		await interaction.reply(`역할 <@&${role.id}> 의 색을 변경하였습니다.`);
+		await interaction.reply(
+			`역할 <@&${role.id}> 의 색을 변경하였습니다. \`(${previousColor} -> ${color})\``
+		);
 	}
 };
